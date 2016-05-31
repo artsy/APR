823a19a0afcf1dbb60e9398d5236fab11a6ae078
Isac Petruzzi <isac.petruzzi@gmail.com>
2016-05-31 16:30:48 -0400

    deploy codebase - compile and runtime broken

--- chef/site-cookbooks/artsy_apr/attributes/default.rb
+++ chef/site-cookbooks/artsy_apr/attributes/default.rb
@@ -1,3 +1,8 @@
+# nodejs
+override['nodejs']['version'] = '6.2.0'
+override['nodejs']['install_method'] = 'binary'
+override['nodejs']['binary']['checksum']['linux_x64'] = '661dba369c277603fa6d0182c4ea7ff074ba6bacd19171826271f872afd6aaa7'
+
 override['nginx']['default_site_enabled'] = false
 
 default['citadel']['bucket'] = "artsy-citadel"
--- chef/site-cookbooks/artsy_apr/recipes/deploy.rb
+++ chef/site-cookbooks/artsy_apr/recipes/deploy.rb
@@ -1,25 +1,69 @@
 application_name = "apr"
 
-# include_recipe "citadel::default"
-# configuration = citadel["#{node['environment']}/#{application_name}"]
-
 deploy_user = "deploy"
 deploy_home = "/home/#{deploy_user}"
 
-nodejs_npm "apr" do
-  path "#{deploy_home}/current"
-  json true
+directory "#{deploy_home}/.ssh" do
+  owner deploy_user
+  recursive true
+end
+
+include_recipe "citadel::default"
+
+file "#{deploy_home}/.ssh/deploy_key" do
+  content citadel["#{application_name}/deploy_key"]
+  owner deploy_user
+  mode '0600'
+end
+
+cookbook_file "#{deploy_home}/wrap-ssh4git.sh" do
+  source 'wrap-ssh4git.sh'
+  owner deploy_user
+  mode '0755'
+end
+
+package "git"
+
+deploy 'apr' do
+  repo 'git@github.com:artsy/apr.git'
   user deploy_user
+  deploy_to deploy_home
+  action :deploy
+  ssh_wrapper "#{deploy_home}/wrap-ssh4git.sh"
+end
+
+application_env = case node['environment']
+when "production"
+  "prod"
+when "development"
+  "dev"
+end
+
+execute "get-mix-deps" do
+  command "MIX_ENV=#{application_env} yes | mix deps.get"
+  cwd "#{deploy_home}/current"
+  returns [0, 137]
+end
+
+execute "compile-mix-deps" do
+  command "MIX_ENV=#{application_env} yes | mix compile"
+  cwd "#{deploy_home}/current"
+  returns [0, 137]
+end
+
+execute "insall-npm-packages" do
+  command "npm install"
+  cwd "#{deploy_home}/current"
 end
 
 command = "mix phoenix.server"
 environment = {
-  "MIX_ENV" => node['environment']
+  "MIX_ENV" => application_env
 }
 
 supervisor_service application_name do
   user deploy_user
-  directory deploy_home
+  directory "#{deploy_home}/current"
   command command
   stdout_logfile "/var/log/supervisor/#{application_name}.out"
   stderr_logfile "/var/log/supervisor/#{application_name}.err"
