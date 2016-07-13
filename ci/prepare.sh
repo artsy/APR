0d831ed67bf2c6df7ee5d7858a1230a871c0d160
Isac Petruzzi <isac.petruzzi@gmail.com>
2016-07-13 16:33:08 -0400

    install erlang / elixir from packages

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -1,61 +1,21 @@
 #!/bin/bash
 
-# Lifted from https://gist.github.com/joakimk/48ed80f1a7adb5f5ea27
-# until CircleCI adds official Elixr/Erlang support https://discuss.circleci.com/t/add-support-for-elixir-erlang/2215
+# CircleCI has no official Elixr/Erlang support https://discuss.circleci.com/t/add-support-for-elixir-erlang/2215
+if [ ! -e /usr/local/bin/elixir ]; then
+  # Install erlang / elixir
+  wget -O /tmp/erlang-solutions_1.0_all.deb https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+  sudo dpkg -i /tmp/erlang-solutions_1.0_all.deb
+  sudo apt-get update
 
-set -e
+  # Workaround, otherwise apt-get install esl-erlang fails
+  sudo rm -rf /etc/rc2.d/K80couchdb
 
-export ERLANG_VERSION="18.3"
-export ELIXIR_VERSION="v1.2.4"
-
-# If you have a elixir_buildpack.config, do this instead:
-#export ERLANG_VERSION=$(cat elixir_buildpack.config | grep erlang_version | tr "=" " " | awk '{ print $2 }')
-#export ELIXIR_VERSION=v$(cat elixir_buildpack.config | grep elixir_version | tr "=" " " | awk '{ print $2 }')
-
-export INSTALL_PATH="$HOME/dependencies"
-
-export ERLANG_PATH="$INSTALL_PATH/otp_src_$ERLANG_VERSION"
-export ELIXIR_PATH="$INSTALL_PATH/elixir_$ELIXIR_VERSION"
-
-mkdir -p $INSTALL_PATH
-cd $INSTALL_PATH
-
-# Install erlang
-if [ ! -e $ERLANG_PATH/bin/erl ]; then
-  curl -OL http://www.erlang.org/download/otp_src_$ERLANG_VERSION.tar.gz
-  tar xzf otp_src_$ERLANG_VERSION.tar.gz
-  cd $ERLANG_PATH
-  ./configure --enable-smp-support \
-              --enable-m64-build \
-              --disable-native-libs \
-              --disable-sctp \
-              --enable-threads \
-              --enable-kernel-poll \
-              --disable-hipe \
-              --without-javac
-  make
-
-  # Symlink to make it easier to setup PATH to run tests
-  ln -sf $ERLANG_PATH $INSTALL_PATH/erlang
+  sudo apt-get install esl-erlang
+  sudo apt-get install elixr
 fi
-
-# Install elixir
-export PATH="$ERLANG_PATH/bin:$PATH"
-
-if [ ! -e $ELIXIR_PATH/bin/elixir ]; then
-  git clone https://github.com/elixir-lang/elixir $ELIXIR_PATH
-  cd $ELIXIR_PATH
-  git checkout $ELIXIR_VERSION
-  make
-
-  # Symlink to make it easier to setup PATH to run tests
-  ln -sf $ELIXIR_PATH $INSTALL_PATH/elixir
-fi
-
-export PATH="$ERLANG_PATH/bin:$ELIXIR_PATH/bin:$PATH"
 
 # Install package tools
 if [ ! -e $HOME/.mix/rebar ]; then
-  yes Y | LC_ALL=en_GB.UTF-8 mix local.hex
-  yes Y | LC_ALL=en_GB.UTF-8 mix local.rebar
+  mix local.hex --force
+  mix local.rebar --force
 fi
