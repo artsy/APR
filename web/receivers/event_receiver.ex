ea3ad03f7926f327247823e6d22f1223463943a3
ashkan <ashkan@artsymail.com>
2016-07-18 11:04:49 -0400

    cleanup

--- web/receivers/event_receiver.ex
+++ web/receivers/event_receiver.ex
@@ -1,3 +1,4 @@
+require IEx
 defmodule Apr.EventReceiver do
   require Logger
 
@@ -5,16 +6,23 @@ defmodule Apr.EventReceiver do
 
   def start_link(channel) do
     KafkaEx.create_worker(String.to_atom(channel))
-    for message <- KafkaEx.stream(channel, 0, worker_name: String.to_atom(channel), offset: latest_offset), acceptable_message?(message.value) do
+    for message <- KafkaEx.stream(channel, 0, worker_name: String.to_atom(channel), offset: latest_offset(channel)), acceptable_message?(message.value) do
       proccessed_message = process_message message
       # broadcast a message to a channel
       Endpoint.broadcast("#{channel}", proccessed_message["verb"], proccessed_message)
     end
   end
 
-  def latest_offset do
-    List.first(List.first(List.first(KafkaEx.latest_offset("users", 0)).partition_offsets).offset)
+  def latest_offset(channel) do
+    IEx.pry
+    KafkaEx.latest_offset(channel, 0)
+      |> List.first
+      |> Map.get(:partition_offsets)
+      |> List.first
+      |> Map.get(:offset)
+      |> List.first
   end
+
   def acceptable_message?(message) do
     try do
       Poison.decode!(message)
