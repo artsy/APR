1808bb40efd75c2776352355ca44fd230ede5494
ashkan <ashkan@artsymail.com>
2016-07-18 10:49:44 -0400

    Fix weird consuming issue by start consuming from latest offset

--- web/receivers/event_receiver.ex
+++ web/receivers/event_receiver.ex
@@ -5,13 +5,16 @@ defmodule Apr.EventReceiver do
 
   def start_link(channel) do
     KafkaEx.create_worker(String.to_atom(channel))
-    for message <- KafkaEx.stream(channel, 0, worker_name: String.to_atom(channel)), acceptable_message?(message.value) do
+    for message <- KafkaEx.stream(channel, 0, worker_name: String.to_atom(channel), offset: latest_offset), acceptable_message?(message.value) do
       proccessed_message = process_message message
       # broadcast a message to a channel
       Endpoint.broadcast("#{channel}", proccessed_message["verb"], proccessed_message)
     end
   end
 
+  def latest_offset do
+    List.first(List.first(List.first(KafkaEx.latest_offset("users", 0)).partition_offsets).offset)
+  end
   def acceptable_message?(message) do
     try do
       Poison.decode!(message)
