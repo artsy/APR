6653de8e3ef695758dc1b30ab0254c444551daff
Ashkan Nasseri <ashkan@artsymail.com>
2018-06-01 19:58:35 -0400

    Some cleanup

--- lib/apr_web/services/incoming_event_service.ex
+++ lib/apr_web/services/incoming_event_service.ex
@@ -7,17 +7,12 @@ defmodule AprWeb.IncomingEventService do
         # fetch user location
         user_task = Task.async(fn -> AprWeb.Gravity.get!("/v1/user/#{event["properties"]["inquirer"]["id"]}") end)
         # fetch partner locations
+        
         artwork = Task.await(artwork_task)
         partner_task = Task.async(fn -> AprWeb.Gravity.get!("/v1/partner/#{artwork.body["partner"]["_id"]}") end)
         partner_locations_task = Task.async(fn -> AprWeb.Gravity.get!("/v1/partner/#{artwork.body["partner"]["_id"]}/locations") end)
-        user = Task.await(user_task)
-        partner = Task.await(partner_task)
-        partner_locations = Task.await(partner_locations_task)
-        IO.puts(topic)
-        IO.puts(routing_key)
-        data = Map.merge(event, %{ partner: partner.body, partner_location: partner_locations.body, artwork: artwork.body, user: user.body})
-        IO.inspect data
-        AprWeb.Endpoint.broadcast(topic, routing_key, event)
+        data = Map.merge(event, %{ "partner": Task.await(partner_task).body, "partner_locations": Task.await(partner_locations_task).body, "artwork": artwork.body, "user": Task.await(user_task).body})
+        AprWeb.Endpoint.broadcast(topic, routing_key, data)
     end
   end
 end
